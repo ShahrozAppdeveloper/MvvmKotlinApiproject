14f1dfa7d439323e605eedf012f6239b10b90c89
Shahroz <shahrozemuhammad198@gmail.com>
2024-01-29 18:59:13 +0500

    ApiDataFetchintoRecyclerview

---
+++ app/src/main/java/com/example/gitmvvmplusapicombineinkotlin/Shehroz/Mvvm/MvvmGetDataActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gitmvvmplusapicombineinkotlin.Shehroz.Mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gitmvvmplusapicombineinkotlin.R
+
+class MvvmGetDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mvvm_get_data)
+    }
+}
